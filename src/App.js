fbf7e49ec30b245cd9e4e08a8260ce52984f430e
emeka <emeka@pop-os.localdomain>
2023-05-30 11:08:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import GenerateCatFact from "./components/GenerateCatFact";
 
 function App() {
+  const [catFacts, setCatFacts] = useState([]);
+
+  const generateCatFact = async () => {
+    try {
+      const res = await fetch("https://catfact.ninja/fact");
+      const data = await res.json();
+      // console.log(data)
+
+      // Send POST request to your JSON server endpoint
+      const response = await fetch("http://localhost:5000/cat-facts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const savedData = await response.json();
+
+      setCatFacts([...catFacts, savedData]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const getFacts = async () => {
+      try {
+        const dataFromServer = await fetchFactsFromServer();
+        setCatFacts(dataFromServer);
+      } catch (error) {
+        console.error("Error retrieving data: ", error.message)
+      }      
+    };
+
+    getFacts();
+  }, []);
+
+  const fetchFactsFromServer = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/cat-facts`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch data from server');
+      }
+      const data = res.json();
+      return data;
+    } catch {
+      console.error("Data not fetched from server");
+      return [];
+    }
+  };
+
+  const deleteCatFact = async (id) => {
+    // delete from DATABASE
+    await fetch(`http://localhost:5000/cat-facts/${id}`, {
+      method: "DELETE",
+    });
+
+    // delete from UI
+    setCatFacts(catFacts.filter((cat) => cat.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <GenerateCatFact
+        generate={generateCatFact}
+        catFacts={catFacts}
+        onDelete={deleteCatFact}
+      />
     </div>
   );
 }
