45969a54cfc930e4fbe0d5e1f026633672717304
emeka <emeka@pop-os.localdomain>
2023-05-31 12:43:29 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CatApp from "./app/catApp/CatApp";
+import ExcuseApp from "./app/excuseApp/ExcuseApp";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/catfacts" element={<CatApp />} />
         <Route path="/" element={<h1>This is my React Apps Page</h1>} />
+        <Route path="/catfacts" element={<CatApp />} />
+        <Route path="/excuse" element={<ExcuseApp />} />
+
+        <Route path="*" element={<h2>Page not Found!!!</h2>} />
       </Routes>
     </Router>
   );
